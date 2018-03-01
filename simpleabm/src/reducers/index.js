1b182c7634cea7471c171c457fc0a2c4c943814b
Nicolas Garcia <a.nicolasgar@gmail.com>
2018-03-01 18:05:33 -0300

    avanzando

--- simpleabm/src/reducers/index.js
+++ simpleabm/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import ContactReducer from './contact-reducer';
+import GroupReducer from './group-reducer';
 import ParticipantReducer from './participant-reducer';
 
 const reducers = {
   contactStore: ContactReducer,
+  groupStore: GroupReducer,
   participantStore: ParticipantReducer
 }
 
